211a821a752ba5e6679b12108d7a948d7f163f9b
agsiv25 <sivarajah@wisc.edu>
2024-11-05 23:50:02 -0600

    ollama server working

--- server/fastapi/app.py
+++ server/fastapi/app.py
@@ -9,7 +9,7 @@ def home():
 
 @app.get('/prompt')
 def ask(prompt :str):
-    res  = requests.post('http://localhost:11434/api/generate', json={
+    res  = requests.post('http://ollamav:11434/api/generate', json={
         "prompt": prompt,
         "stream": False,
         "model" : "bakllava"
